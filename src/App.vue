a350f35fca77e275ce4cf189ae6f50c3afb85c73
changw <2283992824@qq.com>
2022-10-23 10:05:29 +0800

    单选按钮

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-<!--label将input包住，点击label就相当于点击input-->
-  <label for="checkbox">
-    <input type="checkbox" v-model="x">
-    <span>x: {{ x }}</span>
+  <!--label将input包住，点击label就相当于点击input-->
+  你最爱：{{x}}
+  <label>
+    <input name="want" type="radio" v-model="x" :value="1">
+    <span>苹果</span>
+  </label>
+  <label>
+    <input name="want" type="radio" v-model="x" :value="2">
+    <span>番茄</span>
+  </label>
+  <label>
+    <input name="want" type="radio" v-model="x" :value="3">
+    <span>菠萝蜜</span>
   </label>
 </template>
-
 <script>
-
 export default {
   name: 'App',
   data() {
-    return{
-      x: true
+    return {
+      x: ''
     }
   },
   components: {}
