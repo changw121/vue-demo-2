a9ab7992d3e53471ec92be0a03c5ab8ae7b52e1b
changw <2283992824@qq.com>
2022-10-23 09:34:06 +0800

    多行文本的双向绑定事件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <textarea v-model="message" placeholder="edit me"/>
+  <p>Message is: {{ message }}</p>
+  <p><button @click="message = 'changw'">set message to changw</button></p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return{
+      message: 'hi'
+    }
+  },
+  components: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
