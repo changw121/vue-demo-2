57cd713c402585b2ac893cd12f897c1380bb2a68
changw <2283992824@qq.com>
2022-10-23 10:41:43 +0800

    form表单监听回车事件ï¼；保è¿存åform表单数据

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <!--label将input包住，点击label就相当于点击input-->
-  你最爱：{{x}}
-  <select v-model="x">
-<!--    <option value>-</option>-->
-<!--    <option :value="1">苹果</option>-->
-<!--    <option :value="2">香蕉</option>-->
-<!--    <option :value="3">橘子</option>-->
-
-<!--    用v-for渲染-->
-    <option v-for="item in array" :value="item.value" :key="item.value">{{item.text}}</option>
-  </select>
+  <!--  @submit.prevent监听submit事件并阻止默认动作-->
+  登录
+  <form @submit.prevent="onSubmit">
+    <label>
+      <span>用户名</span>
+      <input type="text" v-model="user.username">
+    </label>
+    <label>
+      <span>密码</span>
+      <input type="password" v-model="user.password">
+    </label>
+    <button type="submit">注册</button>
+  </form>
 </template>
 <script>
 export default {
   name: 'App',
   data() {
     return {
-      // 选择框中的数据来自数组（后台）
-      array: [
-        {text: '苹果', value: 1},
-        {text: '香蕉', value: 2},
-        {text: '橘子', value: 3},
-      ],
-      x: []
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user)
     }
   },
   components: {}
