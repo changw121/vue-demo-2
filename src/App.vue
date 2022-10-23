db5e58cb4863ed02d0d4a1e84b84b134e921b4b8
changw <2283992824@qq.com>
2022-10-23 10:20:38 +0800

    选选择框å·：选项来自数组，通过v-for渲染

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <!--label将input包住，点击label就相当于点击input-->
   你最爱：{{x}}
-  <label>
-    <input name="want" type="radio" v-model="x" :value="1">
-    <span>苹果</span>
-  </label>
-  <label>
-    <input name="want" type="radio" v-model="x" :value="2">
-    <span>番茄</span>
-  </label>
-  <label>
-    <input name="want" type="radio" v-model="x" :value="3">
-    <span>菠萝蜜</span>
-  </label>
+  <select v-model="x">
+<!--    <option value>-</option>-->
+<!--    <option :value="1">苹果</option>-->
+<!--    <option :value="2">香蕉</option>-->
+<!--    <option :value="3">橘子</option>-->
+
+<!--    用v-for渲染-->
+    <option v-for="item in array" :value="item.value" :key="item.value">{{item.text}}</option>
+  </select>
 </template>
 <script>
 export default {
   name: 'App',
   data() {
     return {
-      x: ''
+      // 选择框中的数据来自数组（后台）
+      array: [
+        {text: '苹果', value: 1},
+        {text: '香蕉', value: 2},
+        {text: '橘子', value: 3},
+      ],
+      x: []
     }
   },
   components: {}
